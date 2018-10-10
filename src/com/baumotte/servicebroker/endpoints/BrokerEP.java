6718f5373f5ddd15b7fc5ee7c094742d6524176f
baumotte <moritz@baumotte.com>
2018-10-10 08:52:15 +0200

    updated Service.class@

--- src/com/baumotte/servicebroker/endpoints/BrokerEP.java
+++ src/com/baumotte/servicebroker/endpoints/BrokerEP.java
@@ -18,14 +18,14 @@ public class BrokerEP {
 	
 	public BrokerEP() {
 		this.services = new ServiceList();
-		services.addService(new Service("localhost:8080/ticketing/rest/tickets/", "ticketing_tickets"));
-		services.addService(new Service("localhost:8080/dbconnector/rest/queries/ticketing", "dbconnector_ticketing"));
+		services.addService(new Service("http://localhost:8080/ticketing/rest/tickets/", "ticketing_tickets"));
+		services.addService(new Service("http://localhost:8080/dbconnector/rest/queries/ticketing", "dbconnector_ticketing"));
 	}
 	
 	@GET
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getService(@PathParam("name") String name) {
+	public Response getService(/*@PathParam("name") String name*/) {
 		return Response.status(Response.Status.OK).entity(services.getService("dbconnector_ticketing")).build();
 	}
 
--- src/com/baumotte/servicebroker/entities/Service.java
+++ src/com/baumotte/servicebroker/entities/Service.java
@@ -2,20 +2,32 @@ package com.baumotte.servicebroker.entities;
 
 public class Service {
 	
-	private final String URL;
-	private final String name;
+	private String URL;
+	private String name;
 	
 	public Service(String URL, String name) {
 		this.URL = URL;
 		this.name = name;
+	}
+	
+	public Service() {
+		//only used for parsing
 	}
 
 	public String getURL() {
 		return URL;
 	}
 
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
